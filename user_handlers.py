fe91317259a44b32213dcb4c4e38e2705bde6f0e
Sp-V1 <145558131+Sp-V1@users.noreply.github.com>
2023-12-24 17:58:07 +0300

    user_handlers v2.0

--- user_handlers.py
+++ user_handlers.py
@@ -1,31 +1,46 @@
 from tg_logger import TelegramBot
-import os
+import subprocess
 
-token = os.getenv('TOKEN')
+token = ""
+
 bot = TelegramBot(token, None).bot
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, f"Привет!\n Я ваш личный помощник для логгирования моделей")
+    user = message.from_user
+
+    bot.send_message(message.chat.id, text=f"Hey, {user.first_name}! I'm your personal assistant for logging models, "
+                                           f"so let's get started!\n\nFirst of all, you should find out your /chat_id, "
+                                           f"which you will specify in the Python project, and then you can run the "
+                                           f"model using the /running_the_model command.\n\nIf anything, call for "
+                                           f"/help, and I will immediately rush to your rescue!")
 
 
 @bot.message_handler(commands=['chat_id'])
 def chat_id(message):
-    bot.send_message(message.chat.id, f"Ваш chat_id: {message.chat.id}")
+    bot.send_message(message.chat.id, f"Your ID:")
+    bot.send_message(message.chat.id, f"{message.chat.id}")
+
+
+@bot.message_handler(commands=['running_the_model'])
+def running_the_model(message):
+    # subprocess.run(['python', 'example.py'])
+
+    bot.send_message(message.chat.id, f"<i>~Starting model training...</i>", parse_mode='HTML')
 
 
 @bot.message_handler(commands=['help'])
 def help(message):
-    bot.send_message(message.chat.id, f"Вот список команд:\n"
-                                      f"/start - начало работы\n"
-                                      f"/chat_id - узнать свой chat_id\n"
-                                      f"/help - помощь")
+    bot.send_message(message.chat.id, f"Of course! Here is a list of possible commands:\n\n"
+                                      f"/start - Getting started\n\n"
+                                      f"/chat_id - Find out your Chat ID\n\n"
+                                      f"/help - Help")
 
 
 @bot.message_handler(content_types=['text'])
 def text(message):
-    bot.send_message(message.chat.id, f"Вы хотите от меня невозможного")
+    bot.send_message(message.chat.id, f"The Answer to the Ultimate Question of Life, the Universe, and Everything is 42")
 
 
 bot.polling()
